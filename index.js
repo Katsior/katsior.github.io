5663a35286ae2f4dcdeb4f6a90da7c92173c0807
Katsior <rendo@rendo.be>
2016-05-25 17:33:32 +0200

    25052016

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function main() {
 	"use strict";
-
+	document.getElementById("bird_body").addEventListener("click", function(){this.classList.toggle("selected");});
+	document.getElementById("bird_eye").addEventListener("click", function(){this.classList.toggle("selected");});
+	document.getElementById("bird_wing").addEventListener("click", function(){this.classList.toggle("selected");});
+	document.getElementById("bird_snout").addEventListener("click", function(){this.classList.toggle("selected");});
 }
 
 window.onload = function() {
