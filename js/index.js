04351995a7e44c45cef9cde0060897362292914a
Katsior <rendo@rendo.be>
2016-05-26 12:50:44 +0200

    26052016

--- js/index.js
+++ js/index.js
@@ -4,6 +4,15 @@ function main() {
 	document.getElementById("bird_eye").addEventListener("click", function(){this.classList.toggle("selected");});
 	document.getElementById("bird_wing").addEventListener("click", function(){this.classList.toggle("selected");});
 	document.getElementById("bird_snout").addEventListener("click", function(){this.classList.toggle("selected");});
+	
+	$(document).scroll(function() {
+		  var y = $(this).scrollTop();
+		  if (y > 500) {
+			$('.bottomMenu').fadeIn();
+		  } else {
+			$('.bottomMenu').fadeOut();
+		  }
+	});
 }
 
 window.onload = function() {
