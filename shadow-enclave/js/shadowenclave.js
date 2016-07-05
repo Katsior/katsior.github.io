96e26b382b362315b98dd4f95ef7974c82d00c0d
Katsior <rendo@rendo.be>
2016-07-05 12:47:57 +0200

    5072016

--- shadow-enclave/js/shadowenclave.js
+++ shadow-enclave/js/shadowenclave.js
@@ -7,6 +7,28 @@ function main(){
 		$("#intro").fadeOut(4000);
 	}, 5000);
 	
+	$("#left-event").on("click", function(){
+		if ($("#left").hasClass("toggled")){
+		$("#left").css("width","45%");
+		$("#left").css("z-index","2");
+		$("#left").removeClass("toggled");
+		} else {
+			$("#left").css("width","90%");
+			$("#left").css("z-index","100");
+		$("#left").addClass("toggled");
+		}});
+	
+	$("#right-event").on("click", function(){
+		if ($("#right").hasClass("toggled")){
+		$("#right").css("width","45%");
+		$("#right").css("z-index","2");
+		$("#right").removeClass("toggled");
+		} else {
+			$("#right").css("width","90%");
+			$("#right").css("z-index","100");
+		$("#right").addClass("toggled");
+		}});
+	
 }
 
 $(document).ready(function() {
