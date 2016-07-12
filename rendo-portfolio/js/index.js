15f3946184c8f13b81be25e94c07df118dbec304
Katsior <rendo@rendo.be>
2016-07-12 17:31:51 +0200

    ff

--- rendo-portfolio/js/index.js
+++ rendo-portfolio/js/index.js
@@ -1,7 +1,19 @@
-jQuery(document).ready(function() {
-	"use strict";
-    
+function openNav() {
+	$("#nav").css("width", "100%");
+}
+
+function closeNav() {
+	$("#nav").css("width", "0%");
+}
+
+function introAnim() {
 	$("#top-img").css("transform","translate(0%,0)");
 	$("#content").css("margin-top","-100px");
 	$("#top-img h1").css("transform","translate(0%,0)");
+}
+
+jQuery(document).ready(function() {
+	"use strict";
+	introAnim();
+
 });
